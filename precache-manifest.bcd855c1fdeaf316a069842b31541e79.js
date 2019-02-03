4f17a3ed7baf4b500342de3721d4b680fbdfe530
antonwy <anton.wyrowski@gmail.com>
2019-02-03 13:38:54 +0100

    Updates

---
+++ precache-manifest.bcd855c1fdeaf316a069842b31541e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "838012b38c26eb3ee76d",
+    "url": "/wiki-web/static/css/main.481c97db.chunk.css"
+  },
+  {
+    "revision": "838012b38c26eb3ee76d",
+    "url": "/wiki-web/static/js/main.838012b3.chunk.js"
+  },
+  {
+    "revision": "aae86950e81b2fcea5b7",
+    "url": "/wiki-web/static/js/1.aae86950.chunk.js"
+  },
+  {
+    "revision": "dd450eb574c6329e30ce",
+    "url": "/wiki-web/static/js/runtime~main.dd450eb5.js"
+  },
+  {
+    "revision": "2e32fbcb60495511aec012414ee4229b",
+    "url": "/wiki-web/static/media/search-icon.2e32fbcb.svg"
+  },
+  {
+    "revision": "2f3f829d108bb1016fde8c3f28d3a2d5",
+    "url": "/wiki-web/index.html"
+  }
+];
